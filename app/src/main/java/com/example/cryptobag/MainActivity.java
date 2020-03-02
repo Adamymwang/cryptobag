1b749bb29c5474d7565f0454fc5dc51fd9b5e68f
Adamymwang <57311234+Adamymwang@users.noreply.github.com>
2020-03-03 10:56:22 +1100

    Initial commit

---
+++ app/src/main/java/com/example/cryptobag/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.cryptobag;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    public static final String EXTRA_MESSAGE = "au.edu.unsw.infs3634.beers.MESSAGE";
+    private Button mButton;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mButton = findViewById(R.id.btnLaunchActivity);
+
+        mButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                launchDetailActivity("Hello From Detail Activity");
+            }
+        });
+    }
+
+
+    private void launchDetailActivity(String message){
+        Intent intent = new Intent( this, DetailActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
